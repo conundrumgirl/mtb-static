689ba867a5b738154362a3966ea839a17ec2b1ae
Alina Gendel <alina.gendel@sagebase.org>
2022-06-23 14:09:31 -0700

    make hight equal

--- src/components/home/Science.tsx
+++ src/components/home/Science.tsx
@@ -30,31 +30,32 @@ const Science: FunctionComponent<{}> = () => {
         columnSpacing={{xs: 4, lg: 12}}>
         <Grid item xs={12} lg={6}>
           <Typography variant="body1" mb={10} component="div">
-            Mobile Toolbox delivers cognitive and other health measures 
-            developed by content experts and validated for remote administration. 
-            Our growing library of digital measures is derived from well-established 
-            measurement systems such as the NIH Toolbox®, Patient Reported Outcomes Measurement Information System (PROMIS ®), 
-            and International Cognitive Ability Resource (ICAR). 
-            Built on top of Sage Bionetwork’s Mobile Health Platform that has supported 
-            500k research participants, Mobile Toolbox provides tools for
-            automated iOS and Android app configuration, study management, and data collection. 
+            Mobile Toolbox delivers cognitive and other health measures
+            developed by content experts and validated for remote
+            administration. Our growing library of digital measures is derived
+            from well-established measurement systems such as the NIH Toolbox®,
+            Patient Reported Outcomes Measurement Information System (PROMIS ®),
+            and International Cognitive Ability Resource (ICAR). Built on top of
+            Sage Bionetwork’s Mobile Health Platform that has supported 500k
+            research participants, Mobile Toolbox provides tools for automated
+            iOS and Android app configuration, study management, and data
+            collection.
           </Typography>
         </Grid>
         <Grid item xs={12} lg={6}>
           <Typography variant="body1" mb={10} component="div">
-            Mobile Toolbox values transparency. We provide 
-            detailed descriptions of our methods and open access 
-            to our validation data. Convergent validity evidence with gold standard measures 
-            has been collected among healthy adults ages 20-85. 
-            Additional clinical validation and norming 
-            is currently underway including collection of norms and 
-            6-, 12- 18- and 24-month change scores are being calculated 
-            using an age-stratified (20-85), iOS vs Android, national sample 
-            matching the 2020 US Census for gender, race, ethnicity, SES and 
-            level of education.  Clinical validation is in-progress (2022-2023)
-            for 6 different studies in healthy and clinical samples, including 
-            persons at risk for MCI or AD, cognitively impaired, 
-            and those with Parkinson’s disease. 
+            Mobile Toolbox values transparency. We provide detailed descriptions
+            of our methods and open access to our validation data. Convergent
+            validity evidence with gold standard measures has been collected
+            among healthy adults ages 20-85. Additional clinical validation and
+            norming is currently underway including collection of norms and 6-,
+            12- 18- and 24-month change scores are being calculated using an
+            age-stratified (20-85), iOS vs Android, national sample matching the
+            2020 US Census for gender, race, ethnicity, SES and level of
+            education. Clinical validation is in-progress (2022-2023) for 6
+            different studies in healthy and clinical samples, including persons
+            at risk for MCI or AD, cognitively impaired, and those with
+            Parkinson’s disease.
           </Typography>
         </Grid>
 
@@ -63,8 +64,13 @@ const Science: FunctionComponent<{}> = () => {
             {' '}
             <Icon2 />
           </IconHolder>
-          <Typography component="p" mb={10} variant="largeBodyCopy">
-            Convergent validity evidence with gold standards has been collected for Mobile Toolbox.
+          <Typography
+            component="p"
+            mb={10}
+            variant="largeBodyCopy"
+            sx={{height: '80px'}}>
+            Convergent validity evidence with gold standards has been collected
+            for Mobile Toolbox.
           </Typography>
           <Box mb={10}>
             <Button variant="contained" color="primary" href="/assessments">
@@ -78,12 +84,15 @@ const Science: FunctionComponent<{}> = () => {
             {' '}
             <Icon1 />
           </IconHolder>
-          <Typography component="p" variant="largeBodyCopy" mb={10}>
+          <Typography
+            component="p"
+            variant="largeBodyCopy"
+            mb={10}
+            sx={{height: '80px'}}>
             Remote assessments have been created by content experts.
           </Typography>
           <Box mb={10}>
             <Button variant="contained" color="primary" href="/about-us">
-              {' '}
               {' '}
               View Our Contributors
             </Button>
@@ -96,8 +105,12 @@ const Science: FunctionComponent<{}> = () => {
             <Icon3 />
           </IconHolder>
 
-          <Typography component="p" variant="largeBodyCopy">
-            We transparently publish our methods and data for sharing within the research community.
+          <Typography
+            component="p"
+            variant="largeBodyCopy"
+            sx={{height: '80px'}}>
+            We transparently publish our methods and data for sharing within the
+            research community.
           </Typography>
           <Typography
             component="p"
